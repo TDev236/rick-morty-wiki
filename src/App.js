b67b50ef4e14772c040f395e7190da7cd95992ea
Tiffani <tiffanirengifo23@gmail.com>
2022-04-05 10:59:08 -0500

    segundo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import React , { useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import 'bootstrap/dist/js/bootstrap';
 import Cards from './components/Cards/Cards';
 import Filters from './components/Filters/Filters';
 import Pagination from './components/Pagination/Pagination';
+import Search from './components/Search/Search';
 
 function App() {
   const [pageNumber, setPageNumber] = useState(1);
-
-  console.log(pageNumber)
+  const [search, setSearch] = useState("");
   const [fetchedData, updateFetchedData] = useState([]);
   const {info, results} = fetchedData;
-  const api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  const api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
 
   useEffect(() => {
     (async function(){
       let data = await fetch(api).then(res => res.json())
       updateFetchedData(data);
     })()
-  },[api])
-  
+  },[api]);
   return (
     <>
       <div>
         <h1 className='text-center my-4'>
           Rick & Morty - <span className='text-primary'>Wiki</span> 
         </h1>
+        <Search setSearch={setSearch}/>
         <div className="container">
           <div className="row">
             <div className="col-3">
