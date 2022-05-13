b80c9a68138aa44ead8913e2c5664fa5678ee7df
king tiffani <tiffanirengifo23@gmail.com>
2022-05-13 17:39:31 -0500

    projectComplete

--- src/Pages/Location.jsx
+++ src/Pages/Location.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Cards from '../components/Cards/Cards';
+import InputGroup from '../components/Filters/Category/InputGroup';
+
 
 const Location = () => {
-  return (
-    <div>The Locations are Here</div>
+  const [id, setID] = useState(3)
+  const [fetchedData, setFetchedData] = useState([]);
+  const [results, setResults] = useState([])
+  const { type, name, dimension} = fetchedData
+  
+  const api = `https://rickandmortyapi.com/api/location/${id}`
+  useEffect(() => {
+    (async ()  => {
+      const data = await fetch(api).then((res) => res.json());
+      console.log(data);
+      setFetchedData(data);
+
+      const promesa = await Promise.all(
+        data.residents.map((links) => {
+          return fetch(links).then(res => res.json())
+        })
+      );
+      setResults(promesa)
+    })()
+  } , [api])
+  return(
+    <>
+      <div className="container">
+        <div className="row">
+          <h1 className="text-center mb-3">
+            Lugar: { name === "" ? "Desconocido" : <span className='text-primary'>{name}</span>}
+          </h1>
+          <h4 className='text-center mb-4'>
+            Dimension : {dimension === "" ? "Desconocida" :<span className='text-danger'>{dimension}</span>}
+          </h4>
+          <h5 className='text-center mb-4'>
+            Tipo : {type === "" ? "Desconocida" : type}
+          </h5>
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <h4 className="text-center mb-4">
+              Escoger Localizacion
+            </h4>
+            <InputGroup setID={setID} total={126} name="Lugar"/>
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards page="/location/" results={results}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Link } from 'react-router-dom';
 import styles from './Cards.module.scss'
 
-const Cards = ({ results }) => {
+const Cards = ({ results, page }) => {
   let display;
   if(results){
     display = results.map((personaje) => {
       const {name, id, status, image, location} = personaje;
       console.log(id);
       return(
-        <div key={id} className="col-4 mb-4 position-relative">
+        <Link
+          style={{textDecoration: "none"}} 
+          to={`${page}${id}`} 
+          key={id} 
+          className="col-4 mb-4 position-relative text-dark"
+          >
           <div className={`${styles.cards}`}>
             <img src={image} alt="" className={`${styles.img} img-fluid`} />
             <div className={`${styles.content} content`}>
@@ -34,7 +40,7 @@ const Cards = ({ results }) => {
               )
             }
           })()}
-        </div>
+        </Link>
       )
     })
   } else {
